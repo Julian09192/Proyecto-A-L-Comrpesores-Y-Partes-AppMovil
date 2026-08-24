ff80891f491559b3fa83f139f5d9232951c5f390
dilan_w <dilanjwilchesh@gmail.com>
2026-08-24 10:42:46 -0500

    closes #6: desarrollo de vista de productos externos terminada

---
+++ proyecto_ayl_compresores_app_movil/lib/app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'screens/splash/splash_screen.dart'; 
+import 'screens/home/main_navigation.dart';
+
+class MiApp extends StatelessWidget {
+  const MiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'A&L Compresores y Partes',
+      debugShowCheckedModeBanner: false, 
+      theme: ThemeData(
+        primaryColor: Colors.amber, // El amarillo de tu logo
+        scaffoldBackgroundColor: Colors.grey[100], // Un fondo un poco grisáceo para que resalten las tarjetas blancas
+      ),
+      
+      // initialRoute le dice a la app: "Cuando abras, ve directo a esta ruta"
+      initialRoute: '/splash',    
+      
+      // Aquí definimos el "mapa" de las pantallas
+      routes: {
+        '/splash': (context) => const SplashScreen(),
+        '/home': (context) => const MainNavigation(),
+      },
+    );
+  }
+}
