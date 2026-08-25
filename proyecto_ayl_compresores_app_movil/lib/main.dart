5713a0265f140197a719cf3f4fe4e036321b9cd3
24-Luz <luzmarinafrancohernandez@gmail.com>
2026-08-25 11:49:10 -0500

    Arreglo de errores con archivos main.dart y supabase.services.dart

--- proyecto_ayl_compresores_app_movil/lib/main.dart
+++ proyecto_ayl_compresores_app_movil/lib/main.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart'; // Importamos dotenv
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app.dart';
 import 'services/supabase/supabase_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // 1. Cargamos las variables de entorno primero
   await dotenv.load(fileName: ".env");
 
-  // 2. Inicializamos la conexión con Supabase
   await SupabaseService.initialize();
 
   runApp(const MyApp());
--- proyecto_ayl_compresores_app_movil/lib/services/supabase/supabase_service.dart
+++ proyecto_ayl_compresores_app_movil/lib/services/supabase/supabase_service.dart
@@ -3,7 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class SupabaseService {
   static Future<void> initialize() async {
-    // Leemos las llaves del archivo .env
+
     final String supabaseUrl =
         dotenv.env['https://adaiklmjaoajsyixpvgc.supabase.co'] ?? '';
     final String supabaseAnonKey =
@@ -11,10 +11,8 @@ class SupabaseService {
             .env['eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkYWlrbG1qYW9hanN5aXhwdmdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5MzkyMDAsImV4cCI6MjA5MzUxNTIwMH0.XG_N9KAcm0q4QjPl_LzEpRqL4wjyLP2vtklaA5PFvDk'] ??
         '';
 
-    // Inicializamos Supabase
     await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
   }
 
-  // Puedes crear un atajo para usar el cliente en otras partes de tu app
   static final client = Supabase.instance.client;
 }
